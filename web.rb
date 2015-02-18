90382a7d0fcfc8d2219586d674b0b8ba198e2a41
Adam Pash <adam.pash@gmail.com>
2015-02-18 16:47:01 -0500

    removed unnec gems

--- web.rb
+++ web.rb
@@ -1,10 +1,6 @@
 require 'sinatra'
-# require "sinatra/jsonp"
-require 'httparty'
 require 'json'
 require_relative './lib/article'
-
-POST_API = "http://kinja.com/api/core/post"
 
 post '/' do
   params = JSON.parse(request.body.read)
