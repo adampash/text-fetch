4f5c0115c4e2a3c4cc06dab41a1655e3111e4b05
Adam Pash <adam.pash@gmail.com>
2015-02-19 10:13:15 -0500

    Using default sinatra params

--- web.rb
+++ web.rb
@@ -3,14 +3,14 @@ require 'json'
 require_relative './lib/article'
 
 post '/' do
-  puts params
-  params = JSON.parse(request.body.read)
-  puts params
+  # puts params
+  # params = JSON.parse(request.body.read)
+  # puts params
   Article.fetch(params["url"], {format: params["format"]}).to_json
 end
 
 post '/check' do
-  params = JSON.parse(request.body.read)
+  # params = JSON.parse(request.body.read)
   article = Article.fetch(params["url"], {format: 'html'})
   {
     hash: article[:hash]
