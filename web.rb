8443980cd3c70442b459ef6f0547658779446df2
Adam Pash <adam.pash@gmail.com>
2015-02-19 10:09:49 -0500

    Logging params

--- web.rb
+++ web.rb
@@ -3,6 +3,16 @@ require 'json'
 require_relative './lib/article'
 
 post '/' do
+  puts params
   params = JSON.parse(request.body.read)
+  puts params
   Article.fetch(params["url"], {format: params["format"]}).to_json
+end
+
+post '/check' do
+  params = JSON.parse(request.body.read)
+  article = Article.fetch(params["url"], {format: 'html'})
+  {
+    hash: article[:hash]
+  }.to_json
 end
